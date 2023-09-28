f4006e66abb4ce86725a4472585442bca44086b5
ea-boahen <ea.boahen2015@gmail.com>
2023-09-28 19:09:31 +0000

    push first two tasks

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+// You used to attend a place like this at some point
+
+
+export default class ClassRoom { 
+
+	constructor (maxStudentsSize = 0) {
+	this._maxStudentsSize = maxStudentsSize;
+	}
+}
+
+
