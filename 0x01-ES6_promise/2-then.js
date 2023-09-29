6a43bf900f0a248187fd612152bfd7238e367a82
ea-boahen <ea.boahen2015@gmail.com>
2023-09-29 17:29:53 +0000

    fix issue with 1-promise.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,15 @@
 // Promise is handle or not
 function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
-    .catch(() => Error())
-    .finally(() => console.log('Got a response from the API'));
+    return promise
+        .then(response => {
+            // Handle resolution
+            console.log('Got a response from the API');
+            return { status: 200, body: 'success' };
+        })
+        .catch(error => {
+            // Handle rejection
+            return new Error();
+        });
 }
 
 export default handleResponseFromAPI;
