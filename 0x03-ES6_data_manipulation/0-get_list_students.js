87935dd0304a83610026593f980f39d9007612c9
ea-boahen <ea.boahen2015@gmail.com>
2023-10-04 06:47:30 +0000

    fix code errors

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,14 @@
 // Basic list of objects
 
 const getListStudents = () => {
-    const students =  [
-        { id: 1, firstname: 'Guillaume', location: 'San Francisco'}, 
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    
-    ];
+  const students = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
 
-    return students;
-}
+  ];
+
+  return students;
+};
 
 export default getListStudents;
